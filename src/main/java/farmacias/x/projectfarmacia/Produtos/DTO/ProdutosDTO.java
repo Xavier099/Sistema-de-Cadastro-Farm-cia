a714d39bbed4bca2f7eb00cda06e96e3e46f8a33
rique <riquemello89@gmail.com>
2026-02-26 20:32:47 -0300

    feat: Criação das classes DTO e mapper dos produtos

--- src/main/java/farmacias/x/projectfarmacia/Produtos/DTO/ProdutosDTO.java
+++ src/main/java/farmacias/x/projectfarmacia/Produtos/DTO/ProdutosDTO.java
@@ -1,4 +1,19 @@
 package farmacias.x.projectfarmacia.Produtos.DTO;
 
+import farmacias.x.projectfarmacia.User.Model.UserModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProdutosDTO {
+    private Long id;
+    private String nameProduto;
+    private Integer quantidade;
+    private String decricaoProduto;
+    private List<UserModel> userModels;
 }
--- src/main/java/farmacias/x/projectfarmacia/Produtos/DTO/ProdutosMapper.java
+++ src/main/java/farmacias/x/projectfarmacia/Produtos/DTO/ProdutosMapper.java
@@ -1,4 +1,28 @@
 package farmacias.x.projectfarmacia.Produtos.DTO;
 
+import farmacias.x.projectfarmacia.Produtos.Model.ProdutosModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProdutosMapper {
+
+    public ProdutosDTO map(ProdutosModel produtosModel){
+        ProdutosDTO dto = new ProdutosDTO();
+        dto.setId(produtosModel.getId());
+        dto.setNameProduto(produtosModel.getNameProduto());
+        dto.setQuantidade(produtosModel.getQuantidade());
+        dto.setDecricaoProduto(produtosModel.getDescricaoProduto());
+        dto.setUserModels(produtosModel.getUserModels());
+        return dto;
+    }
+
+    public ProdutosModel map(ProdutosDTO produtosDTO){
+        ProdutosModel pdm = new ProdutosModel();
+        pdm.setId(produtosDTO.getId());
+        pdm.setNameProduto(pdm.getNameProduto());
+        pdm.setQuantidade(produtosDTO.getQuantidade());
+        pdm.setDescricaoProduto(produtosDTO.getDecricaoProduto());
+        pdm.setUserModels(produtosDTO.getUserModels());
+        return pdm;
+    }
 }
