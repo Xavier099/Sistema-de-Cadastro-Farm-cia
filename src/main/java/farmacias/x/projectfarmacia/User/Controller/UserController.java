018e23cd78fe244f3c1f25ee45410c702a584c10
rique <riquemello89@gmail.com>
2026-02-27 11:33:36 -0300

    feat: Criação dos endpoints da classe user

---
+++ src/main/java/farmacias/x/projectfarmacia/User/Controller/UserController.java
@@ -0,0 +1,41 @@
+package farmacias.x.projectfarmacia.User.Controller;
+
+import farmacias.x.projectfarmacia.User.DTO.UserDTO;
+import farmacias.x.projectfarmacia.User.Service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //list
+    @GetMapping("/listar")
+    public List<UserDTO> listar(){
+        return userService.list();
+    }
+
+    //listById
+    @GetMapping("/listarId/{id}")
+    public UserDTO listarPorId(@PathVariable Long id){
+        return userService.listByid(id);
+    }
+
+    //criar
+    @PostMapping("/criar")
+    public List<UserDTO> criar(@RequestBody List<UserDTO> userDTO){
+        return userService.createUser(userDTO);
+    }
+    //deletar
+    @DeleteMapping
+    public void deletar(@PathVariable Long id){
+        userService.delete(id);
+    }
+}
