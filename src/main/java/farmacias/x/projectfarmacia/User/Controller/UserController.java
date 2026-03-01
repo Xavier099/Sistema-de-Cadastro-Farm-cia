40a0c4736ef75f104200466f0242c1f1af54228d
rique <riquemello89@gmail.com>
2026-03-01 17:28:29 -0300

    refactor: modificação na classe user controller

--- src/main/java/farmacias/x/projectfarmacia/User/Controller/UserController.java
+++ src/main/java/farmacias/x/projectfarmacia/User/Controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
         return userService.createUser(userDTO);
     }
     //deletar
-    @DeleteMapping
+    @DeleteMapping("/deletar/{id}")
     public void deletar(@PathVariable Long id){
         userService.delete(id);
     }
