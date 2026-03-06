46a25b6bcb420bdc43073eac3ad0b52db73c9907
rique <riquemello89@gmail.com>
2026-03-06 19:25:09 -0300

    reafactor: criação de construtor

--- src/main/java/farmacias/x/projectfarmacia/User/DTO/UserDTO.java
+++ src/main/java/farmacias/x/projectfarmacia/User/DTO/UserDTO.java
@@ -1,6 +1,7 @@
 package farmacias.x.projectfarmacia.User.DTO;
 
 import farmacias.x.projectfarmacia.Produtos.Model.ProdutosModel;
+import farmacias.x.projectfarmacia.User.Model.UserModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,7 @@ public class UserDTO {
     private String cpf;
     private String email;
     private ProdutosModel produtos;
+
+    public UserDTO(UserModel saved) {
+    }
 }
